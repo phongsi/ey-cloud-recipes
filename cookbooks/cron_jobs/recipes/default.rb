2320299b2adbe1725a4bbbfeb8d0aeefae6e998a
Phong Si <phong.si@gmail.com>
2014-01-11 04:34:24 -0500

    see if loaded vars

--- cookbooks/cron_jobs/recipes/default.rb
+++ cookbooks/cron_jobs/recipes/default.rb
@@ -12,7 +12,7 @@ if node[:name] =~ /delayed_job/
   cron "minute tasks" do
     minute '*'
     user 'deploy'
-    command "cd /data/clockwisemd_staging/current && #{page_job} && #{remind_job} && #{late_job}"
+    command "echo TWILIO_AUTH=$TWILIO_AUTH && cd /data/clockwisemd_staging/current && #{page_job} && #{remind_job} && #{late_job}"
   end
 
   cron "2 minute tasks" do
