fff49b89a1aafed7870c634c871067a4bd47e824
Phong Si <phong.si@gmail.com>
2014-01-10 21:43:24 -0500

    heredoc to run cron jobs

--- cookbooks/cron_jobs/recipes/default.rb
+++ cookbooks/cron_jobs/recipes/default.rb
@@ -3,15 +3,27 @@
 # Recipe:: default
 #
 
-queue_page_job = "Delayed::Job.enqueue(DelayedRake.new('sms_callback_patients', nil))"
+page_job = 'Messenger.send_page_messages'
+delay_job = 'Messenger.send_delay_messages'
+remind_job = 'Messenger.send_reminder_messages'
+late_job = 'Messenger.send_late_messages'
 
 if node[:name] =~ /delayed_job/
-  cron "task_name" do
+  cron "minute tasks" do
     minute '1'
     user 'deploy'
     command <<-EOH
       cd /data/clockwisemd_staging/current
-      rails runner -e production "#{queue_page_job}"
+      rails runner -e production '#{page_job} && #{remind_job} && #{late_job}'
+    EOH
+  end
+
+  cron "2 minute tasks" do
+    minute '2'
+    user 'deploy'
+    command <<-EOH
+      cd /data/clockwisemd_staging/current
+      rails runner -e production '#{delay_job}'
     EOH
   end
 end
