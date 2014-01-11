7b845a48c9b22ba968eb86ea580b62a57c5fcd41
Phong Si <phong.si@gmail.com>
2014-01-10 20:11:29 -0500

    load env recipe

--- cookbooks/load_env/recipes/default.rb
+++ cookbooks/load_env/recipes/default.rb
@@ -6,11 +6,11 @@
 execute "load vars and set environment" do
   command <<-EOH
     source /data/clockwisemd_staging/shared/config/env.custom
-    echo $ENCRYPTION_PASSWORD >> /etc/environment
-    echo $MAILER_URL >> /etc/environment
-    echo $SENDGRID_PASSWORD >> /etc/environment
-    echo $SENDGRID_USERNAME >> /etc/environment
-    echo $TWILIO_AUTH >> /etc/environment
-    echo $TWILIO_SID >> /etc/environment
+    echo ENCRYPTION_PASSWORD=$ENCRYPTION_PASSWORD >> /etc/environment
+    echo MAILER_URL=$MAILER_URL >> /etc/environment
+    echo SENDGRID_PASSWORD=$SENDGRID_PASSWORD >> /etc/environment
+    echo SENDGRID_USERNAME=$SENDGRID_USERNAME >> /etc/environment
+    echo TWILIO_AUTH=$TWILIO_AUTH >> /etc/environment
+    echo TWILIO_SID=$TWILIO_SID >> /etc/environment
   EOH
 end
