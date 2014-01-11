0d55c03b5a2da1c468d1d4ed52ea63f99bcf62ad
Phong Si <phong.si@gmail.com>
2014-01-11 04:16:54 -0500

    use rake instead

--- cookbooks/cron_jobs/recipes/default.rb
+++ cookbooks/cron_jobs/recipes/default.rb
@@ -3,21 +3,21 @@
 # Recipe:: default
 #
 
-page_job = 'Messenger.send_page_messages'
-delay_job = 'Messenger.send_delay_messages'
-remind_job = 'Messenger.send_reminder_messages'
-late_job = 'Messenger.send_late_messages'
+page_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_callback_patients'
+delay_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_delay_patients'
+remind_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_reminder_patients'
+late_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_late_patients'
 
 if node[:name] =~ /delayed_job/
   cron "minute tasks" do
     minute '*'
     user 'deploy'
-    command "cd /data/clockwisemd_staging/current && /usr/local/bin/bundle exec rails runner -e production '#{page_job}; #{remind_job}; #{late_job}'"
+    command "cd /data/clockwisemd_staging/current && #{page_job} && #{remind_job} && #{late_job}"
   end
 
   cron "2 minute tasks" do
     minute '*/2'
     user 'deploy'
-    command "cd /data/clockwisemd_staging/current && /usr/local/bin/bundle exec rails runner -e production '#{delay_job}'"
+    command "cd /data/clockwisemd_staging/current && #{delay_job}"
   end
 end
