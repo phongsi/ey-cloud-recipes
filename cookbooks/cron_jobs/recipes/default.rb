828b8ffbcf36018855d728d05a5108118ac77014
Phong Si <phong.si@gmail.com>
2014-01-13 14:16:14 -0500

    dont use env vars

--- cookbooks/cron_jobs/recipes/default.rb
+++ cookbooks/cron_jobs/recipes/default.rb
@@ -3,21 +3,21 @@
 # Recipe:: default
 #
 
-page_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_callback_patients --trace'
-delay_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_delay_patients'
-remind_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_reminder_patients'
-late_job = 'RAILS_ENV=production /usr/local/bin/bundle exec rake sms_late_patients'
+page_job = "/usr/local/bin/bundle exec rails runner -e production 'Messenger.send_page_messages'"
+delay_job = "/usr/local/bin/bundle exec rails runner -e production 'Messenger.send_delay_messages'"
+remind_job = "/usr/local/bin/bundle exec rails runner -e production 'Messenger.send_reminder_messages'"
+late_job = "/usr/local/bin/bundle exec rails runner -e production 'Messenger.send_late_messages'"
 
 if node[:name] =~ /delayed_job/
   cron "minute tasks" do
     minute '*'
     user 'deploy'
-    command "source /data/clockwisemd_staging/shared/config/env.custom && echo TWILIO_AUTH=$TWILIO_AUTH && cd /data/clockwisemd_staging/current && #{page_job} && #{remind_job} && #{late_job}"
+    command "/usr/bin/lockrun --lockfile=/tmp/jobname.lockrun -- sh -c 'cd /data/clockwisemd_staging/current && #{page_job} && #{remind_job} && #{late_job}'"
   end
 
   cron "2 minute tasks" do
     minute '*/2'
     user 'deploy'
-    command "source /data/clockwisemd_staging/shared/config/env.custom && cd /data/clockwisemd_staging/current && #{delay_job}"
+    command "/usr/bin/lockrun --lockfile=/tmp/jobname.lockrun -- sh -c 'cd /data/clockwisemd_staging/current && #{delay_job}'"
   end
 end
